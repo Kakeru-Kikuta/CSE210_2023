d9c5781b30a127129af2d0a246bd595b376deae3
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-01-15 13:01:23 +0900

    1st commit

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Demo
+{   
+    public class Prompt
     {
-        Console.WriteLine("Hello Develop02 World!");
+        public static void Main(String[] args)
+        {
+            Version _promptList = new List<string>
+            _promptList.Add("Who was the most interesting person I interested with todday?");
+            _promptList.Add("What was the best part of my day?");
+            _promptList.Add("How did I see the hand of the Lord in my life today?");
+            _promptList.Add("What was the strongest emotion I felt today?");
+            _promptList.Add("If I had one thing I could do over today, what would it be?");
+            
+            var ramdom = new Random();
+            int index = ramdom.Next(_promptList.Count);
+            Console.WriteLine(_promptList[index]);
+
+        }
+
+    
+
+
     }
+
 }
