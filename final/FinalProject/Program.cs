2e051767c67517f7f81fcfbf54a3c59e93549912
Kakeru Kikuta <kk1911shoter@icloud.com>
2023-02-19 13:19:58 +0900

    ver4

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        int menuUserentry = 0;
+        while (menuUserentry != 2)
+        {
+            Menu menu = new Menu();
+            menu.DsiplayMenu();
+            menuUserentry = menu.DisplayGetMenu();
+
+            TimeCounter num = new TimeCounter();
+
+            Dice rollDice = new Dice();
+
+
+            if (menuUserentry == 2)
+            {
+                break;
+            }
+
+            Console.WriteLine ("Thank you for playing.");
+        }    
+
+        
     }
 }
